8dce8c1c8c990a8550f9120c49e9ef695cf536cb
GreGGus <grportier@gmail.com>
2018-06-05 12:48:06 -0700

    - init NLP tests

---
+++ src/test/scala/NLPTest.scala
@@ -0,0 +1,13 @@
+import org.scalatest.{FunSpec, Matchers}
+import datascience.SentimentDetector
+
+class NLPTest extends FunSpec with Matchers {
+ describe("test nlp") {
+  it("doit avoir le sentiment : POSITIVE") {
+    val input = "On a super équipe, vive la France !"
+    val sentiment = "POSITIVE"
+    sentiment should be("POSITIVE")
+  }
+ }
+
+}
